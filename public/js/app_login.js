af395a89fd4e163cd03b994f9c05005b0257bcf7
YUNA <favoryun12@gmail.com>
2020-11-06 11:07:53 +0900

    login page

---
+++ public/js/app_login.js
@@ -0,0 +1,33 @@
+// 로그인 창 구현
+
+// DOMs
+const $loginPageEnterBtn = document.querySelector(".login-page-enter-btn");
+const $loginwrap = document.querySelector(".login-wrap");
+const $wrap = document.querySelector(".wrap");
+const $loginForm = document.querySelector('.login-form')
+const $loginId = document.querySelector('.login-id');
+const $loginPw = document.querySelector('.login-pw');
+
+
+// function
+const $showPage = () =>{
+  $loginwrap.classList.toggle("active");
+  $wrap.classList.toggle("active");
+}
+
+
+// event handler
+$loginPageEnterBtn.onclick = (e) => { $showPage(); };
+
+$loginForm.onsubmit = e => {
+  e.preventDefalut();
+  if($loginId.value!== && $loginPw.value !==){
+    alert('아이디 또는 비밀번호가 일치하지 않습니다. 다시 입력해주세요')
+  } else {
+    $showPage();
+  }
+};
+
+
+
+
