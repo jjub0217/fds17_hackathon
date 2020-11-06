2fad4ef83dcee8df70c2f93601228727849bcbd9
cjy0019 <cjy0029@naver.com>
2020-11-06 13:06:29 +0900

    feat : todo

--- public/js/app.js
+++ public/js/app.js
@@ -30,6 +30,7 @@ $backgroundBtn.onclick = () => {
   changeBackground();
 };
 backgroundTimer = setTimeout(changeBackground, 3000);
+
 // DOMs
 const $loginPageEnterBtn = document.querySelector(".login-page-enter-btn");
 const $loginwrap = document.querySelector(".login-wrap");
@@ -37,6 +38,8 @@ const $wrap = document.querySelector(".wrap");
 const $loginForm = document.querySelector(".login-form");
 const $loginId = document.querySelector(".login-id");
 const $loginPw = document.querySelector(".login-pw");
+const $loginBtn = document.querySelector(".login-btn");
+
 // function
 const $showPage = () => {
   $loginwrap.classList.toggle("activeLogin");
@@ -47,8 +50,7 @@ $loginPageEnterBtn.onclick = (e) => {
   console.log(1);
   $showPage();
 };
-$loginForm.onsubmit = (e) => {
-  e.preventDefalut();
+$loginBtn.onclick = (e) => {
   if ($loginId.value && $loginPw.value) {
     alert("아이디 또는 비밀번호가 일치하지 않습니다. 다시 입력해주세요");
   } else {
